96fc33e0eb2fb6d9821897a2c3b19f584e027845
Rama <ramiroquesadaperez@gmail.com>
2023-04-29 09:22:53 -0300

    implementacion de validaciones en RegisterForm

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,71 +1,119 @@
+// import { /* useDispatch, */ useSelector } from 'react-redux';
+// import { useMemo } from 'react';
+import { useForm } from '../../hooks/useForm';
 import { Link as RouterLink } from 'react-router-dom';
 import { AuthLayout } from '../layout/AuthLayout';
 import { Grid, TextField, Button, Link, Typography } from '@mui/material';
+// import { checkingAuthentication } from '../../redux/store/auth';
+
+const formData = {
+	email: '',
+	password: '',
+	displayName: '',
+};
+
+const formValidations = {
+	email:    [(value) => value.includes('@'), 'El correo debe tener un @'],
+	password: [(value) => value.length >= 6,
+		'La contraseña debe tener por lo menos 6 caracteres',
+	],
+	displayName: [
+		(value) => value.length >= 3,
+		'El nombre para mostrar debe tener por lo menos 3 caracteres',
+	],
+};
 
 export const RegisterPage = () => {
+	// const dispatch = useDispatch();
+
+	// const { status } = useSelector( state => state.auth)
+
+	// const isAuthenticating = useMemo(()=> status === 'checking', [status])
+
+	const {
+		displayName,
+		email,
+		password,
+		onInputChange,
+		formState,
+		displayNameValid,
+		emailValid,
+		passwordValid,
+		// isFormValid,
+	} = useForm(formData, formValidations);
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+		console.log(formState);
+	};
+
 	return (
-		<AuthLayout title='Crear una Cuenta'>
-			
-			<form>
+		<AuthLayout title="Crear una Cuenta">
+			<form onSubmit={onSubmit}>
 				<Grid container>
-
-					<Grid item xs={ 12 } sx={{ mt: 2 }}>
-
+					<Grid item xs={12} sx={{ mt: 2 }}>
 						<TextField
-							label='Nombre Completo'
+							label="Nombre Completo"
 							type="text"
 							placeholder="Juan Pérez"
-							fullWidth>
-						</TextField>
-
+							fullWidth
+							name="displayName"
+							value={displayName}
+							onChange={onInputChange}
+							error={displayName.length >0 && displayNameValid !== null && displayNameValid !== undefined && displayNameValid !== false}
+							helperText={displayNameValid ? formValidations.displayName[1] : "" }></TextField>
 					</Grid>
-
-					<Grid item xs={ 12 } sx={{ mt: 2 }}>
-
+					<Grid item xs={12} sx={{ mt: 2 }}>
 						<TextField
-							label='Correo'
+							label="Correo"
 							type="email"
 							placeholder="correo@google.com"
-							fullWidth>
-						</TextField>
-
+							fullWidth
+							name="email"
+							value={email}
+							onChange={onInputChange}
+							error={email.length >0 && emailValid !== null && emailValid !== undefined && emailValid !== false}
+							helperText={emailValid ? formValidations.email[1] : "" }></TextField>
 					</Grid>
 
-					<Grid item xs={ 12 } sx={{ mt: 2 }}>
-
+					<Grid item xs={12} sx={{ mt: 2 }}>
 						<TextField
-							autoComplete='true'
-							label='Contraseña'
+							autoComplete="true"
+							label="Contraseña"
 							type="password"
 							placeholder="Contraseña"
-							fullWidth>
-						</TextField>
-
+							fullWidth
+							name="password"
+							value={password}
+							onChange={onInputChange}
+							error={password.length >0 && passwordValid !== null && passwordValid !== undefined && passwordValid !== false}
+							helperText={passwordValid ? formValidations.password[1] : "" }></TextField>
 					</Grid>
 
-					<Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
-
-						<Grid item xs={ 12 }>
-
-							<Button variant="contained" fullWidth>
+					<Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+						<Grid item xs={12}>
+							<Button
+								type="submit"
+								variant="contained"
+								fullWidth>
 								Crear Cuenta
 							</Button>
-
 						</Grid>
-
 					</Grid>
 
-					<Grid container direction='row' justifyContent='end'>
-						<Typography sx={{ mr: 1 }}>¿Ya tienes una cuenta?</Typography>
-						<Link component={ RouterLink } color='inherit' to='/auth/login' >
+					<Grid container direction="row" justifyContent="end">
+						<Typography sx={{ mr: 1 }}>
+							¿Ya tienes una cuenta?
+						</Typography>
+						<Link
+							component={RouterLink}
+							color="inherit"
+							to="/auth/login">
 							Ingresar
 						</Link>
-						
 					</Grid>
-
 				</Grid>
-
 			</form>
 		</AuthLayout>
-	)
-}
+	);
+};
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,25 +1,50 @@
-import { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from 'react';
 
-export const useForm = ( initialForm = {} ) => {
-  
-    const [ formState, setFormState ] = useState( initialForm );
+export const useForm = (initialForm = {}, formValidations = {}) => {
+	const [formState, setFormState] = useState(initialForm);
 
-    const onInputChange = ({ target }) => {
-        const { name, value } = target;
-        setFormState({
-            ...formState,
-            [ name ]: value
-        });
-    }
+	const [formValidation, setFormValidation] = useState({});
 
-    const onResetForm = () => {
-        setFormState( initialForm );
-    }
+	useEffect(() => {
+		createValidators();
+	}, [formState]);
 
-    return {
-        ...formState,
-        formState,
-        onInputChange,
-        onResetForm,
-    }
-}
+	const onInputChange = ({ target }) => {
+		const { name, value } = target;
+		setFormState({
+			...formState,
+			[name]: value,
+		});
+	};
+
+	const onResetForm = () => {
+		setFormState(initialForm);
+	};
+
+	const createValidators = () => {
+		const formCheckedValues = {};
+
+		
+		
+		for (const formField of Object.keys(formValidations)) {
+			
+			const [fn, errorMessage] = formValidations[formField];
+
+			formCheckedValues[`${formField}Valid`] = fn(formState[formField])
+				? null
+				: errorMessage;
+		}
+	
+		setFormValidation(formCheckedValues);
+	};
+
+	return {
+		...formState,
+		formState,
+		onInputChange,
+		onResetForm,
+
+		...formValidation,
+	};
+};
